b6321dbf33c8e2c0ad0035dd483eb8e3f31ddec4
Mateusz Plutowski <mplutowski@p29-16.labpk.inf.ug.edu.pl>
2016-11-05 12:42:11 +0100

    dokonczyc zadanie 7liczb

---
+++ laboratorium/7liczb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+int tab[7];
+int max = 9999;
+int min = 9999;
+}
+
+for (i=0; i<7; i=i +1) {
+  scanf("%d", &tab[i]);
+}
+{
+  for (i<5; i=i+1)
+printf("%d\n", tab[i]);  }
+
+
+
+{  if (tab[i]>max)max=tab[i]
+    printf("max=%d\n" ,max) }
+
+{
+  if (tab[i]<min)min=tab[i] }
+    printf("min=%d" ,min);
+
+{
+
+return 0;
+}
