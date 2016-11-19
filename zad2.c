ae6f59bb24d1c4780cbdd94a22306d38f4236cb2
Mateusz Plutowski <mplutowski@p29-16.labpk.inf.ug.edu.pl>
2016-11-19 12:33:58 +0100

    zad1 do dokonczenia

---
+++ zad2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+int a;
+int b;
+int sum=0;
+
+
+  printf("Podaj 2 liczby całkowite\n");
+  printf("Liczba 1:");
+scanf("%d", &a);
+
+  printf("Liczba 2:");
+scanf("%d", &b);
+
+for "a"
+
+}
+
+
+
+sum += a*b;
+printf("%d\n", sum);
+
+  return 0;
+  }
